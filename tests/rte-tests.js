16805aa42b530cd892343dbc503a532a9cda94b7
Austin Fay <austin.fay@alltechnologysolutions.com>
2016-03-22 14:36:40 -0600

    Changed how tests run to be more segregated

--- tests/rte-tests.js
+++ tests/rte-tests.js
@@ -5,15 +5,30 @@ Tests.results = [];
 
 Tests.RunAll = function()
 {
+    var to = 3000;
+    var i = 0;
+    
+    console.log("//////START TESTS//////");
+
     for (var test in Tests.VariableTests) {
-        if (Tests.VariableTests[test].run)
-            Tests.VariableTests[test].run();
+        if (Tests.VariableTests[test].run) {
+            var runFunc = function () { console.log("Running test: " + this.test + " " + Tests.VariableTests[this.test].template); Tests.VariableTests[this.test].run(); }
+            runFunc = runFunc.bind({test: test});
+            setTimeout(runFunc, to * i);
+            i++;
+        }
     }
 
     for (var test in Tests.TemplateTests) {
-        if (Tests.TemplateTests[test].run)
-            Tests.TemplateTests[test].run();
+        if (Tests.TemplateTests[test].run) {
+            var runFunc = function () { console.log("Running test: " + this.test + " " + Tests.TemplateTests[this.test].template); Tests.TemplateTests[this.test].run(); }
+            runFunc = runFunc.bind({ test: test });
+            setTimeout(runFunc, to * i);
+            i++;
+        }
     }
+
+    setTimeout(function () { console.log("//////END TESTS//////"); }, to * i)
     
 }
 
